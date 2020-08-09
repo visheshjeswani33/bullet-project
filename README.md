bbb43cc474bb74ac66b8b89f6e6185575f5c079c
visheshjeswani33 <64632774+visheshjeswani33@users.noreply.github.com>
2020-08-09 18:29:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bullet-project
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,58 @@
+  
+ var bullet, wall, speed, weight, thickness;
+
+ function setup() {
+  createCanvas(1500,150);
+  
+  speed= random(223,321);
+  weight= random(30,50);
+  thickness= random(22,85);
+
+  bullet=createSprite(50, 80, 10, 50);
+  bullet.shapeColor= "white";
+
+  wall= createSprite(1450,70,20,150);
+  wall.shapeColor= "brown";
+
+  bullet.velocityX= speed;
+
+ }
+
+ function draw() {
+
+  background("lightblue");  
+ 
+  if (collide(bullet,wall)) {
+
+  bullet.velocityX=0;
+  var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+  if(damage>10){
+
+  wall.shapeColor= "red";
+  
+  }
+  if(deformation<10){
+
+  wall.shapeColor= "green";
+  
+  }
+
+  }
+
+  drawSprites();
+
+ }
+
+ function collide (bullet, wall)
+ {
+   
+  bulletREdge= bullet.x + bullet.width;
+  wallLEdge= wall.x;
+  if(bulletREdge>= wallLEdge)
+ {
+  return true;
+
+ }
+  return false;
+  
+ }
